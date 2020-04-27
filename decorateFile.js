2be50bf12ad128fffc46cc1f0080e939fc614faa
MesfinFeleke <51898528+MesfinFeleke@users.noreply.github.com>
2020-04-27 17:19:45 -0500

    decrate

---
+++ decorateFile.js
@@ -0,0 +1,13 @@
+
+
+
+function sum( arr) {
+    let sum = 0;
+   const total = arr.map(function (items) {
+       return sum += items;
+       console.log(total);
+   })
+
+}
+
+ console.log(sum([2,3,5,2])) ;
