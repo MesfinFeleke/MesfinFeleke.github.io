203338c68741b3aae050a4e43ef94671bf3ba9ed
MesfinFeleke <51898528+MesfinFeleke@users.noreply.github.com>
2020-04-30 19:24:16 -0500

    module added

---
+++ module.js
@@ -0,0 +1,14 @@
+
+function showAnswer() {
+   // const AnswerButton = document.getElementById("answer");
+    const textButton = document.getElementById("text");
+    textButton.value = " I am still waiting for solutions ...."
+
+}
+function clearText() {
+  //  const AnswerButton = document.getElementById("clear");
+    const textButton = document.getElementById("text");
+    textButton.value = "" ;
+
+}
+
