859303de0da5ce85c1e5ef8c0f75f7900dec71b1
MesfinFeleke <51898528+MesfinFeleke@users.noreply.github.com>
2020-04-30 22:07:34 -0500

    updated 10

--- module.js
+++ module.js
@@ -1,14 +1,106 @@
 
-function showAnswer() {
-   // const AnswerButton = document.getElementById("answer");
-    const textButton = document.getElementById("text");
-    textButton.value = " I am still working on it ...."
-
+function showAnswer1() {
+    const textButton1 = document.getElementById("text1");
+    textButton1.value = "still working on it ";
 }
-function clearText() {
-  //  const AnswerButton = document.getElementById("clear");
-    const textButton = document.getElementById("text");
-    textButton.value = "" ;
+function showAnswer2() {
+    const textButton2 = document.getElementById("text2");
+    textButton2.value = "still working on it ";
+}
+function showAnswer3() {
+    const textButton3 = document.getElementById("text3");
+    textButton3.value = "still working on it ";
+}
+function showAnswer4() {
+    const textButton4 = document.getElementById("text4");
+    textButton4.value = "still working on it ";
+}
+function showAnswer5() {
+    const textButton5 = document.getElementById("text5");
+    textButton5.value = "still working on it ";
+}
+function showAnswer6() {
+    const textButton6 = document.getElementById("text6");
+    textButton6.value = "still working on it ";
+}
+function showAnswer7() {
+    const textButton7 = document.getElementById("text7");
+    textButton7.value = "still working on it ";
+}
+function showAnswer8() {
+    const textButton8 = document.getElementById("text8");
+    textButton8.value = "still working on it ";
+}
+function showAnswer9() {
+    const textButton9 = document.getElementById("text9");
+    textButton9.value = "still working on it ";
+}
+function showAnswer10() {
+    const textButton10 = document.getElementById("text10");
+    textButton10.value = "still working on it ";
+}
+function showAnswer11() {
+    const textButton11 = document.getElementById("text11");
+    textButton11.value = "still working on it ";
+}
 
+
+
+
+
+function clearText1() {
+  //  const AnswerButton = document.getElementById("clear");
+    const textButton1 = document.getElementById("text1");
+    textButton1.value = ""
+}
+function clearText2() {
+    //  const AnswerButton = document.getElementById("clear");
+    const textButton2 = document.getElementById("text2");
+    textButton2.value = ""
+}
+function clearText3() {
+    //  const AnswerButton = document.getElementById("clear");
+    const textButton3 = document.getElementById("text3");
+    textButton3.value = ""
+}
+function clearText4() {
+    //  const AnswerButton = document.getElementById("clear");
+    const textButton4 = document.getElementById("text4");
+    textButton4.value = ""
+}
+function clearText5() {
+    //  const AnswerButton = document.getElementById("clear");
+    const textButton5 = document.getElementById("text5");
+    textButton5.value = ""
+}
+function clearText6() {
+    //  const AnswerButton = document.getElementById("clear");
+    const textButton6 = document.getElementById("text6");
+    textButton6.value = ""
+}
+function clearText7() {
+    //  const AnswerButton = document.getElementById("clear");
+    const textButton7 = document.getElementById("text7");
+    textButton7.value = ""
+}
+function clearText8() {
+    //  const AnswerButton = document.getElementById("clear");
+    const textButton8 = document.getElementById("text8");
+    textButton8.value = ""
+}
+function clearText9() {
+    //  const AnswerButton = document.getElementById("clear");
+    const textButton9 = document.getElementById("text9");
+    textButton9.value = ""
+}
+function clearText10() {
+    //  const AnswerButton = document.getElementById("clear");
+    const textButton10 = document.getElementById("text10");
+    textButton10.value = ""
+}
+function clearText11() {
+    //  const AnswerButton = document.getElementById("clear");
+    const textButton11 = document.getElementById("text11");
+    textButton11.value = ""
 }
 
