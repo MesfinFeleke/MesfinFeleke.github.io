ea50277d893bff6e7c1c9ced35428c3c8a9dc1d2
MesfinFeleke <51898528+MesfinFeleke@users.noreply.github.com>
2020-05-05 21:14:13 -0500

    classes added

--- inherittance.js
+++ inherittance.js
@@ -1,34 +1,27 @@
 
-/*
-Exercise 1:Define a filterfunction on the Stringobject.
-The function accepts an array of strings that specifies a list of banned words.
-The function returns the string after removing all the banned words.
-Example:console.log("This house is not nice!".filter('not'));Output: "This house is nice!
+
+/*jshint  esversion:6, globalstrict:true*/
 
 
- */
+/*global assert, Account, SavingsAccount, Bank */
 
 
 
-
-/*
-
-Exercise 2:Write a BubbleSortalgorithm on the Array object.Bubble sort is a simple sorting algorithm
-that works by repeatedly stepping through the list to be sorted,
-Example:[6,4,0, 3,-2,1].bubbleSort();Output : [-2, 0, 1, 3, 4, 6]
+function user (name){
+    this.name = name;
 
 
- */
-
-/*
-
-
-
-
- */
-function f() {
-
-var x = 100/”Banana” ;
-console.log(x);
 }
-f();
+
+user.prototype.sayHi =function(){
+    alert(this.name);
+}
+
+let usertwo= new user("mesfin");
+    usertwo.sayHi();
+
+
+
+
+
+
