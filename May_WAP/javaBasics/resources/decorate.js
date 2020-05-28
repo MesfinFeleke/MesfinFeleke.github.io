4502f78463d39eb2946c267268a8f8679a202bd2
MesfinFeleke <51898528+MesfinFeleke@users.noreply.github.com>
2020-05-27 21:40:15 -0500

    checkbox added

--- May_WAP/javaBasics/resources/decorate.js
+++ May_WAP/javaBasics/resources/decorate.js
@@ -1,12 +1,58 @@
 
 
-function a(){
-    return {
-        name: 'George'
-    }
-} // return an object
-function a(){
-    return{
-        name: 'George'
-    }
+// Question
+//Modify your JS function so that it now changes the text area's font size to 24pt
+//sampleTxtArea.style.fontSize ="24pt";
+
+//Use proper units, such as "4em", and proper naming conventions.
+// sampleTxtArea.style.fontSize ="4em";
+
+
+window.onload =function() {
+    "use strict";
+    const decorateBtn = document.getElementById("bigger");
+    decorateBtn.onclick = changeFont;
+    const imgBtn = document.getElementById("imga");
+    imgBtn.onclick = changepic;
+    const blingcheck = document.getElementById("bling");
+    blingcheck.onclick = changeFormate;
+
+
+
 }
+
+function changeFont() {
+    // alert("Hello, world!");
+    const sampleTxtArea = document.getElementById("textarea");
+       sampleTxtArea.style.fontSize = "24pt";
+
+  //  if (sampleTxtArea.style.fontSize === "") {
+   //     sampleTxtArea.style.fontSize = "24pt";
+  //  }
+ //  sampleTxtArea.style.fontSize = parseInt(sampleTxtArea.style.fontSize) + 2 + "pt";
+}
+
+function changepic(){
+    const imgBtn = document.getElementById("imga");
+    imgBtn.src = "resources/shrek2.jpeg";
+}
+
+function changeFormate() {
+    const blingcheck = document.getElementById("bling");
+    const sampleTxtArea = document.getElementById("textarea");
+    if(blingcheck.checked){
+           
+    }
+    else {
+        sampleTxtArea.style.color = "green";
+        sampleTxtArea.style.fontWeight = "bold";
+        sampleTxtArea.style.textDecoration = "underline";
+    }
+
+}
+
+
+
+
+
+
