28e9900dfe70d62b20b5937342bdbd318d279099
MesfinFeleke <51898528+MesfinFeleke@users.noreply.github.com>
2020-06-02 16:17:56 -0500

    added module

--- May_WAP/closure/resources/closure.js
+++ May_WAP/closure/resources/closure.js
@@ -1,3 +1,41 @@
+function pageLoad() {
+
+    "use strict";
+    const createButton = document.getElementById("create");
+    createButton.onclick = createNewAccount;
+
+    const accountCreater = (function () {
+        let acctName;
+        let amount;
+
+        return {
+              createAcccount: function (name, amt) {
+                acctName = name;
+                amount = amt;
+                return {
+                    name: acctName, balance: amount
+                };
+            }
+        }
+    })();
+
+    const accountList = [];
+
+    function createNewAccount() {
+        const newAccount = accountCreater.createAcccount(document.getElementById("account_name").value,
+            document.getElementById("amount").value);
+        accountList.push(newAccount);
+        document.getElementById("display").value = "";
+        for (const account of accountList) {
+            document.getElementById("display").value += `Account name: ${account.name}, Balance: ${account.balance}`
+
+        }
+
+    }
+}
+window.onload = pageLoad;
+
+
 
 
 
--- index/index.js
+++ index/index.js
@@ -1,12 +1,14 @@
 
 
-window.open("index/myAlert.html", "My pop up Window",
-    "width=400,height=400,scrollbars=1");
+" use strict" ;
+window.onload= function () {
+    window.open("index/myAlert.html", "My pop up Window",
+        "width=600,height=600,scrollbars=1");
 
-var dt = new Date();
-document.getElementById("datetime").innerHTML = dt.toLocaleString();
-    ""+ (("0"+dt.getHours()+1).slice(-2)) +":"+ (("0"+dt.getMinutes()+1).slice(-2));
+    var dt = new Date();
+    document.getElementById("datetime").innerHTML = dt.toLocaleString();
+    "" + (("0" + dt.getHours() + 1).slice(-2)) + ":" + (("0" + dt.getMinutes() + 1).slice(-2));
 
-
+};
 
 
