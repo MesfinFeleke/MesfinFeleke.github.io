e4dc97c70b9e058de2da6ee87945732462cd4f57
MesfinFeleke <51898528+MesfinFeleke@users.noreply.github.com>
2020-06-03 14:46:14 -0500

    added wap

--- May_WAP/inheritance/resources/inheritance.js
+++ May_WAP/inheritance/resources/inheritance.js
@@ -1,5 +1,5 @@
 
-
+/*
 const createBicyclePrototye  = {
 
      speed : 0 ;
@@ -13,5 +13,16 @@ const createBicyclePrototye  = {
 } ;
 
  const createMountainBikeProtoype  ={
-     
+
  }
+ */
+window.onload = (function(){
+    let allparagraph = document.querySelector("p");
+    for (let i = 0; i < allparagraph.length; i++) {
+        allparagraph[i].style.backgroundColor = "red";
+
+    }
+})();
+
+
+// $("p").css("background-color", "yellow");
