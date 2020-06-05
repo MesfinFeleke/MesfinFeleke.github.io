45e750a1c53bd54b9e224fcaa742d8cd6fe750fe
MesfinFeleke <51898528+MesfinFeleke@users.noreply.github.com>
2020-06-05 01:18:28 -0500

    new look

--- May_WAP/JQuery/bouncingBall/resources/ball.js
+++ May_WAP/JQuery/bouncingBall/resources/ball.js
@@ -24,15 +24,3 @@ function update() {
     });
 }
 
-    $(function() {
-        $(document).keypress(cheer);
-    });
-    function cheer(e) {
-        $("<li>")
-            .text(String.fromCharCode(e.which).toUpperCase() + "!")
-            .appendTo($("#cheers"));
-        setTimeout(removeCheer, 2000);
-    }
-    function removeCheer() {
-        $("#cheers li").first().remove();
-    }
--- index/index.js
+++ index/index.js
@@ -1,14 +1,16 @@
 
 
-" use strict" ;
+/*
 window.onload= function () {
     window.open("index/myAlert.html", "My pop up Window",
         "width=600,height=600,scrollbars=1");
+*/
+
 
     var dt = new Date();
     document.getElementById("datetime").innerHTML = dt.toLocaleString();
     "" + (("0" + dt.getHours() + 1).slice(-2)) + ":" + (("0" + dt.getMinutes() + 1).slice(-2));
 
-};
+
 
 
